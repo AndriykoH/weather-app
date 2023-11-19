45aaf08220d330c1dc84177b51e208e1ef76ef36
Andriy <77618488+AndriykoH@users.noreply.github.com>
2023-11-19 17:41:20 +0200

    Deploy to GitHub Pages

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Визначте назву репозиторію та ваше ім'я користувача на GitHub
+REPO="weather-app"
+USERNAME="AndriykoH"
+
+# Створіть нову гілку для розгортання
+git checkout -b gh-pages
+
+# Виконайте збірку проекту Vue.js
+npm run build
+
+# Перемістіть зібраний вміст в папку dist
+mv dist/* .
+
+# Додайте зміни та створіть коміт
+git add .
+git commit -m "Deploy to GitHub Pages"
+
+# Залогіньтеся в GitHub з використанням ваших облікових даних
+git remote set-url origin https://github.com/$USERNAME/$REPO.git
+
+# Залогіньтеся до вашого акаунту GitHub, щоб виконати пуш в GitHub Pages
+git push --force origin gh-pages
+
+# Поверніться до головної гілки
+git checkout -
+
+# Видаліть гілку gh-pages (необов'язково)
+git branch -D gh-pages
